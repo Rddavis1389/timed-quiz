25478a1544bb9f8eefd80332e524200c6a5ddaf8
Rddavis1389 <rddavis1389@yahoo.com>
2021-06-06 21:33:20 -0600

    changes to questions

--- script.js
+++ script.js
@@ -14,24 +14,27 @@ THEN I can save my initials and score */
 var startBtn = document.querySelector("#startBtn");
 var timer = document.querySelector("#timer");
 var time = 60;
-var questionIndex = 0;
+var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+var questionIndex = 0
+
+// Questions for the quiz
 var questions = [
-    { q: 'The sky is blue.' a: 't' },
-    { q: 'There are 365 days in a year.', a: 't' },
-    { q: 'There are 42 ounces in a pound.', a: 'f' },
-    { q: 'The Declaration of Independence was created in 1745.', a: 'f' },
-    { q: 'Bananas are vegatables.', a: 'f' }
+    { q: 'The sky is blue.' a: ['t', 'f'], correct: 't'},
+    { q: 'There are 365 days in a year.', a: ['t', 'f'], correct: 't'},
+    { q: 'There are 42 ounces in a pound.', a: ['t', 'f'], correct: 'f'},
+    { q: 'The Declaration of Independence was created in 1745.', a: ['t', 'f'], correct: 'f'},
+    { q: 'Bananas are vegetables.', a: ['t', 'f'], correct: 'f'}
 ];
 
-// start the game with a score of 09
+// start the game with score of 0.
 var score = 0;
 
-// Loop over every question object
+//Loop over every question with a score of 0.
 for (var i = 0; i < questions.length; i++) {
-    // Display current question to user and adk OK/Cancel
-    var answer = confirm(question[i].q);
+    //Display current question to user and ask OK/Cancel
+    var answer = confirm(qustions[i].q);
 
-    // Compare answers
+    //Compare answers
     if (
         (answer === true && question[i].a === 't') ||
         (answer === false && question[i].a === 'f')
@@ -45,8 +48,8 @@ for (var i = 0; i < questions.length; i++) {
     }
 }
 
-// Show totak at end
-alert('You got ' + score + '/' = question.length);
+// Show total at end
+alert('You got ' + score + '/' + questions.length);
 
 startBtn.addEventListener("click", function(){
 
@@ -57,7 +60,7 @@ startBtn.addEventListener("click", function(){
     }, 1000)
 
     //Display the question to questionTitlee
-    console.log(questions[questionIndex].question)
+    console.log(questions[0].question)
     //Display each answer to the answers div (create & append)
 
 })
